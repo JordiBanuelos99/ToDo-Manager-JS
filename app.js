79a8165957dd67ace0a7298bd7f6b765a8c25dc3
BanuelosMendezJordi <jordi.banuelos17@tectijuana.edu.mx>
2022-05-23 22:11:45 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//Global variables
+let newTask = document.getElementById("txtTask"); //Travel the DOM once
+let count = 0;
+
+function registerTask(){
+    console.log(newTask.value);
+    let tmp=`<li id="${count}" class="task">${newTask.value} <button onclick="deleteTask(${count})">🗑️</button> </li>`;
+    document.getElementById("todoList").innerHTML+=tmp;
+    clearForm();
+    count = count + 1;
+}
+function deleteTask(x){
+    document.getElementById(x).remove();
+}
+function clearForm(){
+    newTask.value="";
+}
